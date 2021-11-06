c0b5b28cc6e87a0c6bde72a35b26371c0bc4c5af
DanielPhilosoph <danielph9@gmail.com>
2021-11-06 11:01:21 +0200

    2022 error statistics

--- front/index.js
+++ front/index.js
@@ -1,176 +1,22 @@
 // ==============================
-// ==== Server requests =========
-// ==============================
-
-async function getInfo(shortUrl) {
-  try {
-    let response = await axios.get(
-      `http://localhost:3000/api/shorturl/info/${shortUrl}`
-    );
-    return response.data;
-  } catch (error) {
-    throw error.hasOwnProperty("data")
-      ? error.response.data
-      : "Connection problem";
-  }
-}
-
-async function getShortUrl(fullUrl) {
-  try {
-    let response = await axios.post("http://localhost:3000/api/shorturl/", {
-      url: fullUrl,
-    });
-    return response.data.shorturlId;
-  } catch (error) {
-    throw error.hasOwnProperty("data")
-      ? error.response.data
-      : "Connection problem";
-  }
-}
-
-async function getCustomShortUrl(fullUrl, custom) {
-  try {
-    let response = await axios.post(
-      "http://localhost:3000/api/shorturl/custom",
-      {
-        url: fullUrl,
-        custom: custom,
-      }
-    );
-    console.log(response);
-    return response.data.custom;
-  } catch (error) {
-    throw error.hasOwnProperty("data")
-      ? error.response.data
-      : "Connection problem";
-  }
-}
-
-// ==============================
-// ====== Add Events ============
-// ==============================
-
-document.querySelector("#activate").addEventListener("click", onActivateClick);
-document.querySelector("#copyToClip").addEventListener("click", onCopyClick);
-document
-  .querySelector("#copyToClipCustom")
-  .addEventListener("click", onCopyCustomClick);
-
-document
-  .querySelector("#activateCustom")
-  .addEventListener("click", onActivateCustomClick);
-document
-  .querySelector("#getStatisticsMonth")
-  .addEventListener("click", onStatisticsMonthClick);
-document
-  .querySelector("#getStatisticsYear")
-  .addEventListener("click", onStatisticsYearClick);
-
-// ==============================
-// ====== Event Listeners =======
-// ==============================
-
-async function onActivateClick() {
-  try {
-    resetErrors();
-    let url = document.querySelector("#url").value;
-    if (isUrlValid(url)) {
-      let shortUrl = await getShortUrl(url);
-      document.querySelector("#result").value = "";
-      document.querySelector("#result").value =
-        "http://localhost:3000/api/shorturl/" + shortUrl;
-    }
-  } catch (error) {
-    if (typeof error === "string") {
-      document.querySelector("#urlError").textContent = "Connection problem";
-    } else {
-      document.querySelector("#urlError").textContent = error.message;
-    }
-  }
-}
-
-function onCopyCustomClick() {
-  copyToClipBoard("resultCustom");
-  resetErrors();
-}
-
-function onCopyClick() {
-  copyToClipBoard("result");
-  resetErrors();
-}
-
-async function onActivateCustomClick() {
-  try {
-    resetErrors();
-    let url = document.querySelector("#urlCustomInput").value;
-    if (isUrlValid(url)) {
-      let custom = document.querySelector("#castonShortUrlInput").value;
-      let short = await getCustomShortUrl(url, custom);
-      document.querySelector("#resultCustom").value = "";
-      document.querySelector("#resultCustom").value =
-        "http://localhost:3000/api/shorturl/" + short;
-    }
-  } catch (error) {
-    if (typeof error === "string") {
-      document.querySelector("#customError").textContent = "Connection problem";
-    } else {
-      document.querySelector("#customError").textContent = error.message;
-    }
-  }
-}
-
-async function onStatisticsMonthClick() {
-  try {
-    resetErrors();
-    let shortUrl = document.querySelector("#satisticInput").value;
-    validShortUrl(shortUrl);
-    let shortUrlInfo = await getInfo(shortUrl);
-    let date = new Date(document.querySelector("#satisticInputMonth").value);
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let yearLogsArray = getLogsByYear(shortUrlInfo.redirectEntriesLog, year);
-    let monthName = date.toLocaleString("en-US", { month: "long" });
-    let header = `Entries for year ${year} in ${monthName}`;
-    document.querySelector(".div8").classList.add("visible");
-    createChart(header, getColumnsDays, yearLogsArray, "Days", year, month);
-  } catch (error) {
-    if (typeof error === "string") {
-      document.querySelector("#statistisError").textContent =
-        "Connection problem";
-    } else {
-      document.querySelector("#statistisError").textContent = error.message;
-    }
-  }
-}
-
-async function onStatisticsYearClick() {
-  try {
-    resetErrors();
-    let shortUrl = document.querySelector("#satisticInput").value;
-    validShortUrl(shortUrl);
-    let shortUrlInfo = await getInfo(shortUrl);
-    let year = document.querySelector("#satisticInputYear").value;
-    let yearLogArray = getLogsByYear(shortUrlInfo.redirectEntriesLog, year);
-    let header = `Entries for year ${year}`;
-    document.querySelector(".div8").classList.add("visible");
-    createChart(header, getColumnsMonth, yearLogArray, "Month", year);
-  } catch (error) {
-    if (typeof error === "string") {
-      document.querySelector("#statistisError").textContent =
-        "Connection problem";
-    } else {
-      document.querySelector("#statistisError").textContent = error.message;
-    }
-  }
-}
-
-// ==============================
-// ====== Help Functions ========
+// ======== Function ============
 // ==============================
 
 function validShortUrl(shortUrl) {
   if (shortUrl === "") {
     throw { message: "Short url cant be null" };
+  }
+}
+
+function validCustom(custom) {
+  if (custom === "") {
+    throw { message: "Custom name cant be null" };
+  }
+}
+
+function validYear(year) {
+  if (year < 2020 || year >= 2030) {
+    throw { message: "Year should be between 2020 - 2030" };
   }
 }
 
@@ -196,23 +42,14 @@ function isUrlValid(url) {
   }
 }
 
-function resetErrors() {
-  document.querySelector("#statistisError").textContent = "";
-  document.querySelector("#customError").textContent = "";
-  document.querySelector("#urlError").textContent = "";
-}
-
-function copyToClipBoard(inputId) {
-  let resultInput = document.querySelector(`#${inputId}`);
-  resultInput.select();
-  resultInput.setSelectionRange(0, 99999);
-  navigator.clipboard.writeText(resultInput.value);
-}
-
 function getLogsByYear(logsArray, year) {
-  return logsArray.map((date) => {
-    if (new Date(date).getFullYear() === parseInt(year)) return new Date(date);
-  });
+  let returnArray = [];
+  for (date of logsArray) {
+    if (new Date(date).getFullYear() === parseInt(year)) {
+      returnArray.push(new Date(date));
+    }
+  }
+  return returnArray;
 }
 
 function createChart(
@@ -270,4 +107,189 @@ function getColumnsDays(logArray, year, month) {
     array.push({ label: day, y: countDayEntries(logArray, month, day).length });
   }
   return array;
+}
+
+// ==============================
+// ====== Add Events ============
+// ==============================
+
+document.querySelector("#activate").addEventListener("click", onActivateClick);
+document.querySelector("#copyToClip").addEventListener("click", onCopyClick);
+document
+  .querySelector("#copyToClipCustom")
+  .addEventListener("click", onCopyCustomClick);
+
+document
+  .querySelector("#activateCustom")
+  .addEventListener("click", onActivateCustomClick);
+document
+  .querySelector("#getStatisticsMonth")
+  .addEventListener("click", onStatisticsMonthClick);
+document
+  .querySelector("#getStatisticsYear")
+  .addEventListener("click", onStatisticsYearClick);
+
+// ==============================
+// ====== Event Listeners =======
+// ==============================
+
+async function onActivateClick() {
+  try {
+    resetErrors();
+    let url = document.querySelector("#url").value;
+    if (isUrlValid(url)) {
+      let shortUrl = await getShortUrl(url);
+      document.querySelector("#result").value = "";
+      document.querySelector("#result").value = "/api/shorturl/" + shortUrl;
+    }
+  } catch (error) {
+    if (typeof error === "string") {
+      document.querySelector("#urlError").textContent =
+        "Connection problem /" + error;
+    } else {
+      document.querySelector("#urlError").textContent = error.message;
+    }
+  }
+}
+
+function onCopyCustomClick() {
+  copyToClipBoard("resultCustom");
+  resetErrors();
+}
+
+function onCopyClick() {
+  copyToClipBoard("result");
+  resetErrors();
+}
+
+async function onActivateCustomClick() {
+  try {
+    resetErrors();
+    let url = document.querySelector("#urlCustomInput").value;
+    if (isUrlValid(url)) {
+      let custom = document.querySelector("#castonShortUrlInput").value;
+      validCustom(custom);
+      let short = await getCustomShortUrl(url, custom);
+      document.querySelector("#resultCustom").value = "";
+      document.querySelector("#resultCustom").value = "/api/shorturl/" + short;
+    }
+  } catch (error) {
+    if (typeof error === "string") {
+      document.querySelector("#customError").textContent = "Connection problem";
+    } else {
+      document.querySelector("#customError").textContent = error.message;
+    }
+  }
+}
+
+async function onStatisticsMonthClick() {
+  try {
+    resetErrors();
+    let shortUrl = document.querySelector("#satisticInput").value;
+    validShortUrl(shortUrl);
+    let shortUrlInfo = await getInfo(shortUrl);
+    let date = new Date(document.querySelector("#satisticInputMonth").value);
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let yearLogsArray = getLogsByYear(shortUrlInfo.redirectEntriesLog, year);
+    let monthName = date.toLocaleString("en-US", { month: "long" });
+    let header = `Entries for year ${year} in ${monthName}`;
+    document.querySelector(".div8").classList.add("visible");
+    createChart(header, getColumnsDays, yearLogsArray, "Days", year, month);
+  } catch (error) {
+    if (typeof error === "string") {
+      document.querySelector("#statistisError").textContent =
+        "Connection problem";
+    } else {
+      document.querySelector("#statistisError").textContent = error.message;
+    }
+  }
+}
+
+async function onStatisticsYearClick() {
+  try {
+    resetErrors();
+    let shortUrl = document.querySelector("#satisticInput").value;
+    validShortUrl(shortUrl);
+    let shortUrlInfo = await getInfo(shortUrl);
+    let year = document.querySelector("#satisticInputYear").value;
+    validYear(year);
+    let yearLogArray = getLogsByYear(shortUrlInfo.redirectEntriesLog, year);
+    let header = `Entries for year ${year}`;
+    document.querySelector(".div8").classList.add("visible");
+    createChart(header, getColumnsMonth, yearLogArray, "Month", year);
+  } catch (error) {
+    if (typeof error === "string") {
+      document.querySelector("#statistisError").textContent =
+        "Connection problem";
+    } else {
+      document.querySelector("#statistisError").textContent = error.message;
+    }
+  }
+}
+
+// ==============================
+// ====== DOM Functions ========
+// ==============================
+
+function resetErrors() {
+  document.querySelector("#statistisError").textContent = "";
+  document.querySelector("#customError").textContent = "";
+  document.querySelector("#urlError").textContent = "";
+}
+
+function copyToClipBoard(inputId) {
+  let resultInput = document.querySelector(`#${inputId}`);
+  resultInput.select();
+  resultInput.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(resultInput.value);
+}
+
+// ==============================
+// ===== Server Requests ========
+// ==============================
+
+async function getInfo(shortUrl) {
+  try {
+    let response = await axios.get(
+      `http://localhost:3000/api/shorturl/info/${shortUrl}`
+    );
+    return response.data;
+  } catch (error) {
+    throw error.response.hasOwnProperty("data")
+      ? error.response.data
+      : "Connection problem";
+  }
+}
+
+async function getShortUrl(fullUrl) {
+  try {
+    let response = await axios.post("http://localhost:3000/api/shorturl/", {
+      url: fullUrl,
+    });
+    return response.data.shorturlId;
+  } catch (error) {
+    throw error.response.hasOwnProperty("data")
+      ? error.response.data
+      : "Connection problem";
+  }
+}
+
+async function getCustomShortUrl(fullUrl, custom) {
+  try {
+    let response = await axios.post(
+      "http://localhost:3000/api/shorturl/custom",
+      {
+        url: fullUrl,
+        custom: custom,
+      }
+    );
+    console.log(response);
+    return response.data.custom;
+  } catch (error) {
+    console.log(error.response.data);
+    throw error.response.hasOwnProperty("data")
+      ? error.response.data
+      : "Connection problem";
+  }
 }
